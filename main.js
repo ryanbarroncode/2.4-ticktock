a3248095dfff835096188ed30c395a08ad591f7b
Ryan Barron <ryanbarron243@gmail.com>
2017-01-19 18:10:00 -0500

    Clock on page and working.

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 (function(){
   "use strict";
+ var hours = document.querySelector('.clock .hours');
+ var minutes = document.querySelector('.clock .minutes');
+ var seconds = document.querySelector('.clock .seconds');
 
+ var clock = new Date();
+ var hoursCount = clock.getHours();
+ var minutesCount = clock.getMinutes();
+ var secondsCount = clock.getSeconds();
+
+ hours.textContent = hoursCount;
+ minutes.textContent = minutesCount;
+ seconds.textContent = secondsCount;
+ 
 
 
 
@@ -8,4 +20,4 @@
 
 
   //
-});
+}());
