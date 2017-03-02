9e205a4fa3fda99637a962c6e00f8af28dfd732a
Ryan Barron <ryanbarron243@gmail.com>
2017-03-02 14:46:59 -0500

    reworked clock to have complete functionality

--- main.js
+++ main.js
@@ -1,64 +1,52 @@
 (function(){
-  "use strict";
+'use strict';
 
-  var isHovering = false;
-  var clockFace = document.querySelector('.clock');var clockFace = document.querySelector('.clock');
+var clock = document.getElementById('clock');
+var hexColor = document.getElementById('hex-color');
+var isHovering = false;
 
-  clockFace.addEventListener('mouseover', handleMouseover)
-  clockFace.addEventListener('mouseout', handleMouseout)
+function colorClock() {
+  var time = new Date();
 
-  function handleMouseover(){
-   isHovering = true;
+  var hours = ('0' + (time.getHours()%12)).slice(-2);
+  var minutes = ('0' + time.getMinutes()).slice(-2);
+  var seconds = ('0' + time.getSeconds()).slice(-2);
+
+  var hexRed = ('0' + time.getHours().toString(16)).slice(-2);
+  var hexGreen = ('0' + time.getMinutes().toString(16)).slice(-2);
+  var hexBlue = ('0' + time.getSeconds().toString(16)).slice(-2);
+
+  var timeCode = hours + ':' + minutes + ':' + seconds;
+  var hexCode = hexRed + hexGreen + hexBlue;
+
+  // updating clock display
+  if (isHovering) {
+    clock.textContent = hexCode;
+  } else {
+    clock.textContent = timeCode;
   }
 
-  function handleMouseout(){
-   isHovering = false;
+  // updating progress bar
+  var timeLine = seconds / 60 * 100;
+  document.getElementById('bar').style.width = timeLine + "%";
+
+  // updating background color
+  // hexCode.split("") => "1" "2" "3"
+  var reverseHex = hexCode.split("").reverse().join("");
+  document.body.style.backgroundImage = "radial-gradient(ellipse farthest-corner , #" + reverseHex + " 0%, #" + hexCode + " 95%)"
+}
+
+  clock.addEventListener('mouseover', handleMouseOver);
+  clock.addEventListener('mouseout', handleMouseOut);
+
+  function handleMouseOver(){
+    isHovering = true;
   }
 
-
-  function showTime(){
-
-    var clock = new Date(); //new date is a how you start getting the time
-    var h = ('0' + clock.getHours()).slice(-2); //gets the hour value and slice only keeps the two numbers from the right.
-    var m = ('0' + clock.getMinutes()).slice(-2); //gets the minute value / also this is where the ifis hovering grabs the time from
-    var s = ('0' + clock.getSeconds()).slice(-2); //gets the second value
-
-    var hHex = ('0' + clock.getHours().toString(16)).slice(-2);
-    var mHex = ('0' + clock.getMinutes().toString(16)).slice(-2); // this is where the hexvalue gets used in the else statement.
-    var sHex = ('0' + clock.getSeconds().toString(16)).slice(-2);
-
-    var hours = document.querySelector('.clock .hours');//targets the hour in html
-    var minutes = document.querySelector('.clock .minutes');//targets the minute in html
-    var seconds = document.querySelector('.clock .seconds');//targets the seconds in html
-
-
-    if(isHovering){
-      hours.textContent = hHex; //if the mouse is hovering over the clock. show the hex value. isHovering True!!
-      minutes.textContent = mHex;
-      seconds.textContent = sHex;
-    } else {
-      hours.textContent = h; // takes the text content of 00 within the hours html and sets the hourscount to every hour minute or second.
-      minutes.textContent = m;//if isHovering is false or not hovering then it will show the time.
-      seconds.textContent = s;
-    }
-
-
-
-
-    var timeline = s / 60 * 100;// this is the code for making the timeline or the expanding bar move. at a perccentage
-
-
-
-    document.getElementById("timeBar").style.width = timeline + "%";
-
-    var color = '#' + hHex + mHex + sHex; // this is the hex color code.
-
-    document.body.style.backgroundColor = color;// this goes into the html (document) > body > and chooses the css background color to be equal to the variable color.
-    console.log(color);
-
-    // updates the function, showTime every 1000ms or 1 second.
-
+  function handleMouseOut(){
+    isHovering = false;
   }
-  showTime();
-setInterval(showTime, 20);
+
+  window.setInterval(colorClock, 100)
+
 }());
